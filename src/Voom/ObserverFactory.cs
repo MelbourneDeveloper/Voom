35cacb4f18222f6f4ddca2e392a8f37c29321d37
Christian Findlay <a@a.com>
2020-08-29 22:20:53 +1000

    Add the observer factory

---
+++ src/Voom/ObserverFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Voom
+{
+    public class ObserverFactory<T> : IDisposable
+    {
+        private bool isRunning = true;
+        Func<T> _func;
+        int _millisecondsTimeout = 1000;
+
+        public ObserverFactory(
+            Func<T> func,
+            int? millisecondsTimeout = null)
+        {
+            _func = func;
+            if (millisecondsTimeout.HasValue)
+            {
+                _millisecondsTimeout = millisecondsTimeout.Value;
+            }
+        }
+
+        public void Dispose()
+        {
+            isRunning = false;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            Task.Run(() =>
+            {
+                while (isRunning)
+                {
+                    observer.OnNext(_func());
+                    Thread.Sleep(_millisecondsTimeout);
+                }
+            });
+
+            return this;
+        }
+    }
+}
