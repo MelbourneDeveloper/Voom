1344db4521f527e14954f686d7ce60fa0cce4b28
Christian Findlay <a@a.com>
2020-08-30 14:38:16 +1000

    Add a dummy class

---
+++ src/Voom/Observer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Voom
+{
+    /// <summary>
+    /// This thing is probably useless. Use Observer.Create in System.Reactive
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Observer<T> : IObserver<T>
+    {
+        private readonly Action<T> _action;
+
+        public Observer(Action<T> action)
+        {
+            _action = action;
+        }
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(T value) => _action(value);
+    }
+}
