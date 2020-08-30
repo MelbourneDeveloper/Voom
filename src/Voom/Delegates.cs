6ce57f3959c0ed0b6b6e276ef2aa351d43535eab
Christian Findlay <a@a.com>
2020-08-30 17:46:28 +1000

    Add a delegate

--- src/Voom/Delegates.cs
+++ src/Voom/Delegates.cs
@@ -1,8 +1,12 @@
-﻿namespace Voom
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Voom
 {
     public delegate void ValueSet<T>(T value);
     public delegate T ValueGet<T>();
     public delegate void BlankDelegate();
     public delegate void RaisePropertyChanged(string propertyName);
     public delegate bool CheckEquality<T>(T oldValue, T newValue);
+    public delegate Task RunOnDispatcher(Action action);
 }
